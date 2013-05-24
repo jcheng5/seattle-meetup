7e40d4fac39faa5efce89d4367d87b74b17f000e
Joe Cheng <joe@rstudio.com>
2013-05-24 13:38:14 -0700

    Add stamps example

---
+++ stamps/server.R
@@ -0,0 +1,11 @@
+library(BSDA)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+
+  output$hist <- renderPlot({
+    binwidth <- diff(range(Stamp$thickness)) / (input$breaks-1)
+    print(qplot(Stamp$thickness, binwidth=binwidth))
+  })
+
+})
